d6aa698491ff59f1a58a25df91b5f137d17d8aa8
tomasestacio <97453864+tomasestacio@users.noreply.github.com>
2022-03-27 15:51:03 -0700

    Update writenoncanonicalTomas.c

--- writenoncanonicalTomas.c
+++ writenoncanonicalTomas.c
@@ -17,9 +17,10 @@
 #define FALSE 0
 #define TRUE 1
 #define FLAG 0x7E
-#define A 0x03
+#define A_SE 0x03
+#define A_RC 0x01
 #define C 0x03
-#define BCC A^C
+#define BCC A_SE^C
 
 int state=1;
 int tentat=0;
@@ -31,6 +32,7 @@ volatile int STOP=FALSE;
 void control_alarm(){
   STOP = TRUE;
   tentat++;
+  printf("TENT#1: %d\n", tentat);
   return;
 }
 
@@ -92,14 +94,13 @@ int main(int argc, char** argv)
     
     (void) signal(SIGALRM, control_alarm); //controlar o alarme (inserir interrupçao no sistema)
 
-    unsigned char aux[MAX];
     /*printf("Enter a string: ");
     fgets(str, MAX, stdin);
     count = strlen(str)+1;
 
     printf("String: %s\n", str);*/
     buf[0] = FLAG;
-    buf[1] = A;
+    buf[1] = A_SE;
     buf[2] = C;
     buf[3] = BCC;
     buf[4] = FLAG;
@@ -117,6 +118,7 @@ int main(int argc, char** argv)
     }*/
 
     printf("%d bytes written\n", res);
+    printf("SET: 0X%X:0X%X:0X%X:0X%X:0X%X\n", buf[0], buf[1], buf[2], buf[3], buf[4]);
 
     int total=0;
 
@@ -131,85 +133,90 @@ int main(int argc, char** argv)
       printf("%d bytes recebidos\n", nr); 
     }*/
 
-    while(tentat <= 3){
-        switch (state)
-        {   
-          case 1:
-          nr = read(fd, &aux[0], 1);
-          total += nr;
-          if(STOP == TRUE) state = 7;
-          if(aux[0] == buf[0]) state = 2;
-          else tcflush(fd, TCIOFLUSH);
+    switch (state)
+    {   
+      case 1:
+      nr = read(fd, &buf[0], 1);
+      total += nr;
+      if(STOP == TRUE) state = 7;
+      if(buf[0] == (unsigned char) FLAG){
+        printf("OLA: %X\n", buf[0]);
+        state = 2;
+      }
+      else tcflush(fd, TCIOFLUSH);
       
-          case 2:
-          nr = read(fd, &aux[1], 1);
-          total += nr;
-          if(STOP == TRUE) state = 7;
-          if(aux[1] == buf[1]) state = 3;
-          else{
-            tcflush(fd, TCIOFLUSH);
-            state = 1;
-          }
+      case 2:
+      nr = read(fd, &buf[1], 1);
+      total += nr;
+      if(STOP == TRUE) state = 7;
+      if(buf[1] == (unsigned char)A_RC) state = 3;
+      else{
+        tcflush(fd, TCIOFLUSH);
+        state = 1;
+      }
   
-          case 3:
-          nr = read(fd, &aux[2], 1);
-          total += nr;
-          if(STOP == TRUE) state = 7;
-          if(aux[2] == buf[2]) state = 4;
-          else{
-            tcflush(fd, TCIOFLUSH);
-            state = 1;
-          }
+      case 3:
+      nr = read(fd, &buf[2], 1);
+      total += nr;
+      if(STOP == TRUE) state = 7;
+      if(buf[2] == (unsigned char)C) state = 4;
+      else{
+        tcflush(fd, TCIOFLUSH);
+        state = 1;
+      }
 
-          case 4:
-          nr = read(fd, &aux[3], 1);
-          total += nr;
-          if(STOP == TRUE) state = 7;
-          if(aux[3] == buf[3]) state = 5;
-          else{
-            tcflush(fd, TCIOFLUSH);
-            state = 1;
-          }
+      case 4:
+      nr = read(fd, &buf[3], 1);
+      total += nr;
+      if(STOP == TRUE) state = 7;
+      if(buf[3] == (unsigned char)BCC) state = 5;
+      else{
+        tcflush(fd, TCIOFLUSH);
+        state = 1;
+      }
 
-          case 5:
-          nr = read(fd, &aux[4], 1);
-          total += nr;
-          if(STOP == TRUE) state = 7;
-          if(aux[4] == buf[4]) state = 6;
-          else{
-            tcflush(fd, TCIOFLUSH);
-            state = 1;
-          }
+      case 5:
+      nr = read(fd, &buf[4], 1);
+      total += nr;
+      if(STOP == TRUE) state = 7;
+      if(buf[4] == (unsigned char)FLAG) state = 6;
+      else{
+        tcflush(fd, TCIOFLUSH);
+        state = 1;
+      }
 
-          case 6:
-          if(total == 5) break;
-          else if(STOP == TRUE) state = 7;
-          else{
-            tcflush(fd, TCIOFLUSH);
-            state = 1;
-          }
+      case 6:
+      if(total == 5) break;
+      else if(STOP == TRUE) state = 7;
+      else{
+        tcflush(fd, TCIOFLUSH);
+        state = 1;
+      }
         
-          case 7:
-          alarm(3);
-          tcflush(fd, TCIOFLUSH);
-          if(tentat == 3){
-            close(fd);
-            return 0;
-          }
-         }
+      case 7:
+      if(tentat == 3){
+        alarm(3);
+        tcflush(fd, TCIOFLUSH);
+        close(fd);
+        return 0;
+      }
+      alarm(3);
+      tcflush(fd, TCIOFLUSH);
+      state = 1;
     }
     
-    printf("SET: %X:%X:%X:%X:%X\n", aux[0], aux[1], aux[2], aux[3], aux[4]);
   /* 
     O ciclo FOR e as instru��es seguintes devem ser alterados de modo a respeitar 
     o indicado no gui�o 
   */
-   
-    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
 
-    close(fd);
-    return 0;
+  bzero(buf,255); //limpar o char buf
+
+  if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
+
+  close(fd);
+  return 0;
 }
