bc5a081a7c11dd3cbcd33a0a191678af0441e536
tomasestacio <97453864+tomasestacio@users.noreply.github.com>
2022-03-31 17:24:36 +0100

    Add files via upload

---
+++ linklayer.c
@@ -0,0 +1,56 @@
+#ifndef LINKLAYER
+#define LINKLAYER
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+typedef struct linkLayer{
+    char serialPort[50];
+    int role; //defines the role of the program: 0==Transmitter, 1=Receiver
+    int baudRate;
+    int numTries;
+    int timeOut;
+} linkLayer;
+
+//ROLE
+#define NOT_DEFINED -1
+#define TRANSMITTER 0
+#define RECEIVER 1
+
+
+//SIZE of maximum acceptable payload; maximum number of bytes that application layer should send to link layer
+#define MAX_PAYLOAD_SIZE 1000
+
+//CONNECTION deafault values
+#define BAUDRATE_DEFAULT B38400
+#define MAX_RETRANSMISSIONS_DEFAULT 3
+#define TIMEOUT_DEFAULT 4
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+
+//MISC
+#define FALSE 0
+#define TRUE 1
+
+int llopen(linkLayer connectionParameters)
+{
+    if(((connectionParameters.role == 0) || (connectionParameters.role == 1)) &&
+       (connectionParameters.baudrate == BAUDRATE_DEFAULT) && 
+       (connectionParameters.numTries < MAX_RETRANSMISSIONS_DEFAULT) &&
+       (connectionParameters.timeOut < TIMEOUT_DEFAULT)){
+        return 1;
+    }
+    else return -1;
+}
+
+int llwrite(char* buf, int bufSize)
+{
+    
+}
+
