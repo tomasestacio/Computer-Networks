bc1baee579f0bcd274c2bca4dd9f6142cb9de360
tomasestacio <97453864+tomasestacio@users.noreply.github.com>
2022-04-15 14:37:02 -0700

    Update constants.h

--- constants.h
+++ constants.h
@@ -1,4 +1,7 @@
 //constantes hexadecimais
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
 #define FLAG 0x7E
 #define A_TRANS 0x03
 #define A_REC 0x01
@@ -15,10 +18,12 @@ int get_baud(int baud);
 int establishment_trans(); //returns 1 if everything goes well, -1 if error -> include in llwrite
 int establishment_rec(); //returns 1 if everything goes well, -1 if error -> include in llread
 unsigned char informationcheck(); //returns the control flag for the transmitter of the information packet
-unsigned char confirmationcheck(char* packet); //returns the control flag for the receiver of the information packet
+unsigned char confirmationcheck(); //returns the control flag for the receiver of the information packet
 int transmitter_information_write(char* buf, int bufSize); //stuffing included, returns size of trama, if error returns -1
 int transmitter_information_read(); //returns size of trama readed, -1 if error
 int receiver_information_read(); //returns size of trama readed, -1 if error
 int receiver_information_write(char* packet); //returns size of trama, if error returns -1
 int termination_trans(); //returns 1 if everything goes well, -1 if error
 int termination_rec(); //returns 1 if everything goes well, -1 if error
+
+#endif
