582451b12174bb1598994898a76781b0f64c04da
tomasestacio <97453864+tomasestacio@users.noreply.github.com>
2022-06-12 02:23:17 +0100

    Add files via upload

---
+++ lab1/rcom/application/constants.h
@@ -0,0 +1,30 @@
+//constantes hexadecimais
+#include <termios.h>
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define FLAG 0x7E
+#define A_TRANS 0x03
+#define A_REC 0x01
+//C pode ser 0x02 ou 0x00 (information frame)
+#define ESC 0x7D
+#define SET 0x03
+#define DISC 0x0B
+#define UA 0x07
+
+// Opens a conection using the "port" parameters defined in struct linkLayer, returns "-1" on error and "1" on sucess
+
+speed_t get_baud(int baud);
+int establishment_trans(); //returns 1 if everything goes well, -1 if error
+int establishment_rec(); //returns 1 if everything goes well, -1 if error
+unsigned char informationcheck(); //returns the control flag for the transmitter of the information packet
+unsigned char confirmationcheck(); //returns the control flag for the receiver of the information packet
+int transmitter_information_write(char* buf, int bufSize); //stuffing included, returns size of buffer if everything goes ok, if error returns 0
+int transmitter_information_read(); //returns 1 if everything is ok, 0 if REJ was sent and -1 if timeout was activated during the read
+int receiver_information_read(); //returns size of packet if everything is ok, 0 if REJ was sent and -1 if timeout was activated during the read
+int receiver_information_write(char* packet); //returns 1, if error returns 0
+int termination_trans(); //returns 1 if everything goes well, -1 if error
+int termination_rec(); //returns 1 if everything goes well, -1 if error
+
+#endif
+
